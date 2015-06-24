b5f798d16693a92f679318e52bc36d33f4df1686
DerRomtester <dieb.stefan.96@gmail.com>
2015-06-24 18:11:13 +0200

    reduce touchboost frequency
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- ramdisk/sbin/render-post_boot.sh
+++ ramdisk/sbin/render-post_boot.sh
@@ -9,6 +9,7 @@ echo "[Tyr-Kernel] Boot Script Started" | tee /dev/kmsg
 
 # cpufreq settings
 
+echo 1574400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq;
 echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
 echo conservative > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
 echo conservative > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
@@ -16,7 +17,7 @@ echo conservative > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
 echo 90 > /sys/devices/system/cpu/cpufreq/conservative/up_threshold;
 echo 10 > /sys/devices/system/cpu/cpufreq/conservative/freq_step;
 echo 40 > /sys/devices/system/cpu/cpufreq/conservative/down_threshold;
-echo 1728000 > /sys/devices/system/cpu/cpufreq/conservative/input_boost_freq;
+echo 1497600 > /sys/devices/system/cpu/cpufreq/conservative/input_boost_freq;
 echo 1574400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq;
 
 echo "[Tyr-Kernel] Boot Script Completed!" | tee /dev/kmsg
