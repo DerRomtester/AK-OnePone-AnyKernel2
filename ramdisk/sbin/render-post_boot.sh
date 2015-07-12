6f29080ba231355470c711eb7bee223ea96ec3ad
DerRomtester <dieb.stefan.96@gmail.com>
2015-07-12 14:17:24 +0200

    ramdisk: update wifi firmware + some values
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- ramdisk/sbin/render-post_boot.sh
+++ ramdisk/sbin/render-post_boot.sh
@@ -17,7 +17,9 @@ echo conservative > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
 echo 90 > /sys/devices/system/cpu/cpufreq/conservative/up_threshold;
 echo 10 > /sys/devices/system/cpu/cpufreq/conservative/freq_step;
 echo 40 > /sys/devices/system/cpu/cpufreq/conservative/down_threshold;
-echo 1497600 > /sys/devices/system/cpu/cpufreq/conservative/input_boost_freq;
+echo 65 > /sys/class/misc/mako_hotplug_control/load_threshold;
+echo 5 > /sys/class/misc/mako_hotplug_control/high_load_counter;
+echo 1728000 > /sys/devices/system/cpu/cpufreq/conservative/input_boost_freq;
 echo 1574400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq;
 
 echo "[Tyr-Kernel] Boot Script Completed!" | tee /dev/kmsg
