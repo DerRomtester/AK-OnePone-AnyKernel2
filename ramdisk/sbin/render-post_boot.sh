136d7894a064fe305eabb56f5b90da07ddfe6167
DerRomtester <dieb.stefan.96@gmail.com>
2015-07-24 19:30:41 +0200

    anykernel ramdisk: update
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- ramdisk/sbin/render-post_boot.sh
+++ ramdisk/sbin/render-post_boot.sh
@@ -10,16 +10,15 @@ echo "[Tyr-Kernel] Boot Script Started" | tee /dev/kmsg
 # cpufreq settings
 
 echo 1574400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq;
-echo conservative > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
-echo conservative > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
-echo conservative > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
-echo conservative > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
-echo 90 > /sys/devices/system/cpu/cpufreq/conservative/up_threshold;
-echo 10 > /sys/devices/system/cpu/cpufreq/conservative/freq_step;
-echo 40 > /sys/devices/system/cpu/cpufreq/conservative/down_threshold;
-echo 65 > /sys/class/misc/mako_hotplug_control/load_threshold;
-echo 5 > /sys/class/misc/mako_hotplug_control/high_load_counter;
-echo 1728000 > /sys/devices/system/cpu/cpufreq/conservative/input_boost_freq;
+echo smartmax > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+echo smartmax > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
+echo smartmax > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
+echo smartmax > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
+echo 20 > /sys/module/cpu_boost/parameters/boost_ms 20;
+echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold 1728000;
+echo 1497600 > /sys/module/cpu_boost/parameters/input_boost_freq 1497600;
+echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms 40;
+echo bfq > /sys/block/mmcblk0/queue/scheduler;
 echo 1574400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq;
 
 echo "[Tyr-Kernel] Boot Script Completed!" | tee /dev/kmsg
