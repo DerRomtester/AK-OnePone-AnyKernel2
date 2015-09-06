693dc51baaca63ec792b3477155551f84cf9624c
DerRomtester <dieb.stefan.96@gmail.com>
2015-09-06 15:35:50 +0200

    update
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- ramdisk/sbin/render-post_boot.sh
+++ ramdisk/sbin/render-post_boot.sh
@@ -10,14 +10,14 @@ echo "[Tyr-Kernel] Boot Script Started" | tee /dev/kmsg
 # cpufreq settings
 
 echo 1574400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq;
-echo smartmax > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
-echo smartmax > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
-echo smartmax > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
-echo smartmax > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
-echo 20 > /sys/module/cpu_boost/parameters/boost_ms 20;
-echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold 1728000;
-echo 1497600 > /sys/module/cpu_boost/parameters/input_boost_freq 1497600;
-echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms 40;
+echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+echo interactive > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
+echo interactive > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
+echo interactive > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
+echo 20 > /sys/module/cpu_boost/parameters/boost_ms;
+echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold;
+echo 0:1497600 1:960000 2:652800 3:652800 > /sys/module/cpu_boost/parameters/input_boost_freq;
+echo 40 > /sys/module/cpu_boost/parameters/input_boost_ms;
 echo bfq > /sys/block/mmcblk0/queue/scheduler;
 echo 1574400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq;
 
