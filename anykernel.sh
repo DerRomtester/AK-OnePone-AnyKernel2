bb25d744c6718d1159b391225ae847ad28c4df50
DerRomtester <dieb.stefan.96@gmail.com>
2015-10-03 14:19:58 +0200

    Now my CM and my CAF kernel can use the same anykernel ramdisk yeah
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -149,24 +149,25 @@ replace_file() {
 ## AnyKernel install
 dump_boot;
 
-# begin ramdisk changes
-
 # insert initd scripts
 cp -fp $patch/init.d/* $initd
 chmod -R 766 $initd
 
-#
-# Fstab for f2fs
-#
+# adb secure
+backup_file default.prop;
+replace_string default.prop "ro.adb.secure=0" "ro.adb.secure=1" "ro.adb.secure=0";
+replace_string default.prop "ro.secure=0" "ro.secure=1" "ro.secure=0";
 
-backup_file fstab.bacon
-replace_file fstab.bacon 750 fstab.bacon;
+# init.bacon.rc
+backup_file init.bacon.rc;
+append_file init.bacon.rc "render-post_boot" init.bacon.patch;
 
-#
-#init.qcom-common.rc
-#
+# panel and gamma
 backup_file init.qcom-common.rc
-replace_file init.qcom-common.rc 750 init.qcom-common.rc; 
+replace_line init.qcom-common.rc "chown system graphics /sys/devices/virtual/graphics/fb0/panel_calibration" "    chown system system /sys/devices/virtual/graphics/fb0/panel_calibration";
+
+# Disable QCOM Thermal Driver
+insert_line init.qcom-common.rc "#Disable QCOM Thermal" after "service thermal-engine /system/bin/thermal-engine" "   #Disable QCOM Thermal\n   disabled\n"
 
 # end ramdisk changes
 
