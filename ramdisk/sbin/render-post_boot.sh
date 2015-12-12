95cd4393ab87c5813ad8eabf97bd970e90a203d0
DerRomtester <dieb.stefan.96@gmail.com>
2015-12-12 14:47:06 +0100

    update

--- ramdisk/sbin/render-post_boot.sh
+++ ramdisk/sbin/render-post_boot.sh
@@ -1,20 +1,10 @@
 #!/system/bin/sh
 
-BB=/sbin/bb/busybox
-
 ############################
 # Custom Kernel Settings for Tyr Kernel by DerRomtester!!
 # Credits to RenderBroken
 echo "[Tyr-Kernel] Boot Script Started" | tee /dev/kmsg
 stop mpdecision
-
-############################
-# CPU-Boost Settings
-#
-echo 20 > /sys/module/cpu_boost/parameters/boost_ms
-echo 80 > /sys/module/cpu_boost/parameters/input_boost_ms
-echo 0:1497600 1:1497600 2:1497600 3:1497600 > /sys/module/cpu_boost/parameters/input_boost_freq
-echo 1728000 > /sys/module/cpu_boost/parameters/sync_threshold
 
 ############################
 # Scheduler and Read Ahead
@@ -23,64 +13,12 @@ echo bfq > /sys/block/mmcblk0/queue/scheduler
 echo 128 > /sys/block/mmcblk0/bdi/read_ahead_kb
 
 ############################
-# GPU Governor
-#
-echo 578000000 > /sys/devices/fdb00000.qcom,kgsl-3d0/devfreq/fdb00000.qcom,kgsl-3d0/max_freq
-
-############################
 # Governor Tunings
 #
+echo "1497600 1190400" > /sys/kernel/cpu_input_boost/ib_freqs
 echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
 echo interactive > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
 echo interactive > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
 echo interactive > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
-echo 20000 1400000:40000 1700000:20000 > /sys/devices/system/cpu/cpufreq/interactive/above_hispeed_delay
-echo 90 > /sys/devices/system/cpu/cpufreq/interactive/go_hispeed_load
-echo 1190400 > /sys/devices/system/cpu/cpufreq/interactive/hispeed_freq
-echo 1 > /sys/devices/system/cpu/cpufreq/interactive/io_is_busy
-echo 85 1400000:90 1800000:70 > /sys/devices/system/cpu/cpufreq/interactive/target_loads
-echo 40000 > /sys/devices/system/cpu/cpufreq/interactive/min_sample_time
-echo 30000 > /sys/devices/system/cpu/cpufreq/interactive/timer_rate
-echo 100000 > /sys/devices/system/cpu/cpufreq/interactive/max_freq_hysteresis
-echo -1 > /sys/devices/system/cpu/cpufreq/interactive/timer_slack
-echo 0 > /dev/cpuctl/cpu.notify_on_migrate
-
-############################
-# Disable Debugging
-#
-echo "0" > /sys/module/kernel/parameters/initcall_debug;
-echo "0" > /sys/module/alarm_dev/parameters/debug_mask;
-echo "0" > /sys/module/binder/parameters/debug_mask;
-echo "0" > /sys/module/xt_qtaguid/parameters/debug_mask;
-echo "[Tyr-Kernel] disable debug mask" | tee /dev/kmsg
-
-############################
-# Init.d Support
-# 
-/sbin/busybox run-parts /system/etc/init.d
-
-############################
-# SuperUser Tweaks (TESTING)
-# Allow untrusted apps to read from debugfs
-if [ -e /system/lib/libsupol.so ]; then
-/system/xbin/supolicy --live \
-	"allow untrusted_app debugfs file { open read getattr }" \
-	"allow untrusted_app sysfs_lowmemorykiller file { open read getattr }" \
-	"allow untrusted_app persist_file dir { open read getattr }" \
-	"allow debuggerd gpu_device chr_file { open read getattr }" \
-	"allow netd netd capability fsetid" \
-	"allow netd { hostapd dnsmasq } process fork" \
-	"allow { system_app shell } dalvikcache_data_file file write" \
-	"allow { zygote mediaserver bootanim appdomain }  theme_data_file dir { search r_file_perms r_dir_perms }" \
-	"allow { zygote mediaserver bootanim appdomain }  theme_data_file file { r_file_perms r_dir_perms }" \
-	"allow system_server { rootfs resourcecache_data_file } dir { open read write getattr add_name setattr create remove_name rmdir unlink link }" \
-	"allow system_server resourcecache_data_file file { open read write getattr add_name setattr create remove_name unlink link }" \
-	"allow system_server dex2oat_exec file rx_file_perms" \
-	"allow mediaserver mediaserver_tmpfs file execute" \
-	"allow drmserver theme_data_file file r_file_perms" \
-	"allow zygote system_file file write" \
-	"allow atfwd property_socket sock_file write" \
-	"allow debuggerd app_data_file dir search"
-fi;
 
 echo "[Tyr-Kernel] Boot Script Completed!" | tee /dev/kmsg
