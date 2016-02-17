51c1a0e4f1c8e8990435d4db7f460293e782379b
DerRomtester <dieb.stefan.96@gmail.com>
2016-02-17 16:55:27 +0100

    anykernel: update to my latest version
    Signed-off-by: DerRomtester <dieb.stefan.96@gmail.com>

--- ramdisk/sbin/render-post_boot.sh
+++ ramdisk/sbin/render-post_boot.sh
@@ -12,13 +12,9 @@ stop mpdecision
 echo bfq > /sys/block/mmcblk0/queue/scheduler
 echo 128 > /sys/block/mmcblk0/bdi/read_ahead_kb
 
-############################
-# Governor Tunings
-#
-echo "1497600 1190400" > /sys/kernel/cpu_input_boost/ib_freqs
-echo interactive > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-echo interactive > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor
-echo interactive > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor
-echo interactive > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor
+echo "smartmax" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor;
+echo "smartmax" > /sys/devices/system/cpu/cpu1/cpufreq/scaling_governor;
+echo "smartmax" > /sys/devices/system/cpu/cpu2/cpufreq/scaling_governor;
+echo "smartmax" > /sys/devices/system/cpu/cpu3/cpufreq/scaling_governor;
 
 echo "[Tyr-Kernel] Boot Script Completed!" | tee /dev/kmsg
